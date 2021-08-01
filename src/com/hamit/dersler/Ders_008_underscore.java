a0a47096a32044e522f4578f94ea55b729c1d555
HamitMizrak <hamitmizrak@gmail.com>
2021-08-01 12:58:29 +0300

    underscore

---
+++ src/com/hamit/dersler/Ders_008_underscore.java
@@ -0,0 +1,16 @@
+package com.hamit.dersler;
+
+public class Ders_008_underscore {
+
+	public static void main(String[] args) {
+
+		// okunabilirğii artırmak için
+
+		long telefonNumarasi1 = 111222333445566L;
+		System.out.println(telefonNumarasi1);
+
+		long telefonNumarasi2 = 111_222_333_44_55_66L;
+		System.out.println(telefonNumarasi2);
+	}
+
+}
