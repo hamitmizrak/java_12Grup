031d2220be857cf36a5b8c397cbc9f2e2216bc8c
HamitMizrak <hamitmizrak@gmail.com>
2021-08-01 11:30:47 +0300

    psvm anlatıldı

--- src/com/hamit/dersler/Ders_001_psvm.java
+++ src/com/hamit/dersler/Ders_001_psvm.java
@@ -2,6 +2,13 @@ package com.hamit.dersler;
 
 public class Ders_001_psvm {
 
+	// comment
+	// tek satırlık not
+	/*
+	 * asd asd asd asd as d asd as d
+	 * 
+	 */
+
 	public static void main(String[] args) {
 		System.out.println("Hamit Mızrak");
 
