ec054bf7d819c920f0228f5976116af991626085
HamitMizrak <hamitmizrak@gmail.com>
2021-08-01 12:58:21 +0300

    Hatalar

---
+++ src/com/hamit/dersler/Ders_007_hatalar.java
@@ -0,0 +1,19 @@
+package com.hamit.dersler;
+
+public class Ders_007_hatalar {
+
+	public static void main(String[] args) {
+
+		// compiler Time: kodlama sırasındaki hatalardır.
+		// Syntax error
+		// intx sayi=44;
+
+		// logic error
+		// int sayi = 4 / 0; // sayıyı/sıfıra bol = sonsuz yani belirsiz
+		// System.out.println(sayi);
+
+		// run time error
+		// belleğimiz yer kalmadı.
+	}
+
+}
