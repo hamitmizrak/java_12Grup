bf9f60cc7f9c1ceb87437b1816b9ea6fa94ef422
HamitMizrak <hamitmizrak@gmail.com>
2021-08-01 12:58:13 +0300

    bilimsel gösterim

---
+++ src/com/hamit/dersler/Ders_006_bilimsel.java
@@ -0,0 +1,20 @@
+package com.hamit.dersler;
+
+public class Ders_006_bilimsel {
+
+	public static void main(String[] args) {
+
+		double bilimsel1 = 123400;
+		System.out.println(bilimsel1);
+
+		// E+2= 2 tane sıfır sağa kaydır
+		double bilimsel2 = 1234E+2;
+		System.out.println(bilimsel2);
+
+		// E-2= 2 tane sıfır sola kaydır
+		double bilimsel3 = 1234E-2;
+		System.out.println(bilimsel3);
+
+	}
+
+}
