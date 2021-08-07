edad0d19dd6753ab7c9546c77429eef57149e0ad
HamitMizrak <hamitmizrak@gmail.com>
2021-08-07 13:59:31 +0300

    Calendar

---
+++ src/com/hamit/dersler/Ders_021_Calendat.java
@@ -0,0 +1,26 @@
+package com.hamit.dersler;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Ders_021_Calendat {
+
+	// Date
+	// @Deprecated ==> Date
+	public static void main(String[] args) {
+
+		Locale locale = new Locale("tr", "TR");
+		// SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy", locale);
+		// SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MMM-yyyy", locale);
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MMMM.yyyy hh:mm.ss zzzz", locale);
+
+		Calendar calendar = Calendar.getInstance();
+		String cevir = simpleDateFormat.format(calendar.getTime());
+
+		System.out.println(calendar);
+		System.out.println(cevir);
+
+	}
+
+}
