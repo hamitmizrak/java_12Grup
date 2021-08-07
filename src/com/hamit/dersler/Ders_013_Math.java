3cc81bea2cbcfe80ca2e792cc243afa796de7049
HamitMizrak <hamitmizrak@gmail.com>
2021-08-07 12:37:11 +0300

    Math

---
+++ src/com/hamit/dersler/Ders_013_Math.java
@@ -0,0 +1,21 @@
+package com.hamit.dersler;
+
+public class Ders_013_Math {
+
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		System.out.println(Math.E);
+		System.out.println(Math.min(4, -85));
+		System.out.println(Math.max(4, -85));
+		System.out.println("mutlak sayı: " + Math.abs(-144));
+		System.out.println("karekök sayı: " + Math.sqrt(Math.abs(-144)));
+		System.out.println(Math.pow(2, 5));
+		System.out.println(Math.floor(4.9));
+		System.out.println(Math.ceil(4.1));
+
+		System.out.println(Math.round(4.5));
+		System.out.println(Math.round(4.4));
+
+	}
+
+}
