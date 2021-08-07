0889163fe35018061d28043b0d1a0962a21d9212
HamitMizrak <hamitmizrak@gmail.com>
2021-08-07 12:36:59 +0300

    escape

---
+++ src/com/hamit/dersler/Ders_012_escape.java
@@ -0,0 +1,15 @@
+package com.hamit.dersler;
+
+public class Ders_012_escape {
+
+	public static void main(String[] args) {
+		System.out.println("\" \n\tHamit");
+
+		System.err.println("C gelen özellik");
+		System.gc();
+
+		System.out.println(System.currentTimeMillis()); // 1970 1 ocak 00:00 2021
+		System.exit(0); // logout
+	}
+
+}
