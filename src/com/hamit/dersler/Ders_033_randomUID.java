4aca3898f06cf21fb1a51a4a1b271ac4fbece127
HamitMizrak <hamitmizrak@gmail.com>
2021-08-15 12:11:13 +0300

    UUID

---
+++ src/com/hamit/dersler/Ders_033_randomUID.java
@@ -0,0 +1,16 @@
+package com.hamit.dersler;
+
+import java.util.UUID;
+
+public   class Ders_033_randomUID { //bu classı extends edemezsiniz. 
+	
+	public static void main(String[] args) {
+		
+		UUID rastgele=UUID.randomUUID();
+		System.out.println(rastgele);
+		
+	}
+	
+
+}
+
